1fe483b8cd135e5b4b42d7244f2edbeeffb08f2f
Shafayetul Islam <shafatsib@gmail.com>
2024-02-28 23:24:41 +0600

    string

---
+++ Python/problems/test.py
@@ -0,0 +1,9 @@
+a="Hello world"
+
+print(a.upper()) #upper case
+print(a.lower()) #lower case
+print(a.strip()) #removing white space before and after the actual textclea
+print(a.replace("Hello","Hi")) #Replace string
+print(a.split(" ")) #splitting string 
+print("Hello, I'm {} and i'm {} years old".format("Shafayet",26)) #Print string and int inline
+print("I love {car_name} but i can buy {bike_name}".format(car_name="BMW",bike_name="Hero Honda"))
